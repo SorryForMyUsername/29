faf57d1133c3b86e3576994e0ed41dbf6296ed04
Ilya <119899583+SorryForMyUsername@users.noreply.github.com>
2025-03-13 14:38:07 +0200

    Add files via upload

---
+++ Magazine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Magazine : Press
+    {
+        string quality;
+
+        public string Quality { get => quality; set => quality = value; }
+
+        public Magazine(int copies, string name, decimal price, string quality) : base(copies, name, price)
+        {
+            Quality = quality;
+        }
+
+        public override decimal Cost()
+        {
+            
+        }
+
+        public override void Output()
+        {
+            Console.WriteLine($"Кол-во копий: {Copies}\tНазвание: {Name}\tЦена: {Price}\tКачество: {Quality}");
+        }
+    }
+}
---
+++ Press.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Press
+    {
+        int copies;
+        string name;
+        decimal price;
+
+        public int Copies { get => copies; set => copies = value; }
+        public string Name { get => name; set => name = value; }
+        public decimal Price { get => price; set => price = value; }
+
+        public Press() { }
+
+        public Press(int copies, string name, decimal price)
+        {
+            Copies = copies;
+            Name = name;
+            Price = price;
+        }
+
+        public virtual decimal Cost()
+        {
+            return Copies * Price;
+        }
+
+        public virtual void Output()
+        {
+            Console.WriteLine($"Кол-во копий: {Copies}\tНазвание: {Name}\tЦена: {Price}");
+        }
+    }
+}
